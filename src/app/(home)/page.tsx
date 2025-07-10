0510c9dc5b8af1e6bcaf5b864b4cdbfc68a7413a
Reileen00 <business.rout001@gmail.com>
2025-07-10 14:56:26 +0530

    14:home-page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+import { ProjectForm } from "@/modules/home/ui/components/project-form";
+const Page = () => {
+  return (
+    <div className="flex flex-col max-w-5xl mx-auto w-full">
+      <section className="space-y-6 py-[16vh] 2xl:py-48">
+        <div className="flex flex-col items-center">
+          <Image
+            src="/logo.svg"
+            alt="Vibe"
+            width={50}
+            height={50}
+            className="hidden md:block"
+          />
+        </div>
+        <h1 className="text-2xl md:text-5xl fnt-bold text-center">Build something with Vibely</h1>
+        <p className="text-lg md:text-xl text-muted-foreground text-center">Create apps and websites by chatting with AI</p>
+        <div>
+          <ProjectForm/>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Page;
